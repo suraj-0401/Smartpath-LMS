ed9d5d89fc28847dc8644ccad5ba30c68cbf8543
suraj <shekharsuraj201@gmail.com>
2025-03-07 12:40:45 +0530

    ui pages changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React, { Suspense, lazy } from 'react';
-import Spinner from './components/Spinner';
-const Navbar = lazy(() => import('./components/Navbar'));
-const Stats = lazy(() => import('./components/Stats'));
-const Benefits = lazy(() => import('./components/Benefits'));
-const Features = lazy(() => import('./components/Features'));
-const FAQs = lazy(() => import('./components/FAQs'));
-const ContactForm = lazy(() => import('./components/ContactForm'));
-const Footer = lazy(() => import('./components/Footer'));
-const Testimonials = lazy(() => import('./components/Testimonials'));
-const HeroSection = lazy(() => import('./components/HeroSection'));
+import Spinner from './Home/Spinner';
+const Navbar = lazy(() => import('./Home/Navbar'));
+const Stats = lazy(() => import('./Home/Stats'));
+const Benefits = lazy(() => import('./Home/Benefits'));
+const Features = lazy(() => import('./Home/Features'));
+const FAQs = lazy(() => import('./Home/FAQs'));
+const ContactForm = lazy(() => import('./Home/ContactForm'));
+const Footer = lazy(() => import('./Home/Footer'));
+const Testimonials = lazy(() => import('./Home/Testimonials'));
+const HeroSection = lazy(() => import('./Home/HeroSection'));
 
 function App() {
   return (
